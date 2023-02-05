33c1d34b25c475bb73a3aab4d61799bd8561d303
wanerviana <74058990+wanerviana@users.noreply.github.com>
2023-02-05 07:49:25 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+Flask
+render_template
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+
+
+
+lista=[]
+
+@app.route("/")
+def Inicio():
+    return render_template("index.html")
+
+@app.route("/lista.html", methods=["POST"])
+def Lista():
+    produto = 'picanha'
+    lista.append(produto)
+    return render_template("lista.html", msg=lista)
+
+
+
+if __name__ == "__main__":
+        app.run('0.0.0.0')
